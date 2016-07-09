09294571bfb5ac460cba008f25535c7c37b49b26
bo kwon <boyoung@whitesky.com>
2016-07-08 17:41:42 -0700

    add promise structure

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -9,11 +9,24 @@
   
     function AuthController($firebaseAuth){
       var vm = this;
+      var firebaseReference = new Firebase('https://wac-waitandeat.firebaseio.com/');
+      var firebaseAuthObject = $firebaseAuth(firebaseReference);
+      
+      vm.user = {
+        email: '',
+        password: ''
+      };
       
       vm.register = register;
       
       function register(user){
-        
+        return firebaseAuthObject.$createUser(user) //this returns promise
+          .then(function(user){
+            console.log(user);
+          }) // success case
+          .catch(function(error){
+            console.log(error);
+          }); // error case
       }
     }
   
